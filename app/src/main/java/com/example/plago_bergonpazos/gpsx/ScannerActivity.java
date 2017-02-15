12ac52cbe681836acacf2c668c4a95f5d4f8e128
plago-bergonpazos <plago-bergonpazos@danielcastelao.org>
2017-02-15 10:09:46 +0100

    Permisos solicitado: Camara en Scanner Activity

--- app/src/main/java/com/example/plago_bergonpazos/gpsx/ScannerActivity.java
+++ app/src/main/java/com/example/plago_bergonpazos/gpsx/ScannerActivity.java
@@ -1,5 +1,9 @@
 package com.example.plago_bergonpazos.gpsx;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +15,56 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class ScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     private ZXingScannerView mScannerView;
-
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
     @Override
     protected void onCreate(Bundle state) {
         super.onCreate(state);
 
         mScannerView = new ZXingScannerView(this);
         setContentView(mScannerView);
+
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.CAMERA)) {
+
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case MY_PERMISSIONS_REQUEST_CAMERA:{
+                if(grantResults.length<0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+                }else{
+
+                }return;
+            }
+        }
+
     }
 
     @Override
